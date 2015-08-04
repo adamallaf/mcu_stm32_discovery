2a3f5152037578681d4b941a809bfe19197d72cf
adam allaf <allaf.mohamad@gmail.com>
2015-08-04 22:08:43 +0200

    Demo project created

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,19 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f100RB_demo' 
+ * Target:  'STM32F100RB-Discovery' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_I2C
+#define RTE_DEVICE_STDPERIPH_RCC
+#define RTE_DEVICE_STDPERIPH_TIM
+#define RTE_DEVICE_STDPERIPH_USART
+
+#endif /* RTE_COMPONENTS_H */
---
+++ Source Files/main.c
@@ -0,0 +1,19 @@
+/**
+* @file				main.c
+* @Author			Adam Allaf
+* @version  	01.00
+* @date				04-08-2015
+* @brief			STM32F100-discovery demo project
+*/
+
+int main(){
+	
+	while(1){
+	
+	}
+	
+	/*** Safety routine. DON'T MODIFY THIS CODE!!! ***/
+	for(;;);
+	
+	return 0;
+}
